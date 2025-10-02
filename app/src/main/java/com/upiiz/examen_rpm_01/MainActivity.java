da424dac6f401e823761c0556cb09ea4751c8ecb
RobertoPM02 <rpichardomier23@gmail.com>
2025-10-01 18:47:21 -0600

    Modificacion de strings en el main activity y conexion de los botones salir y de creditos

--- app/src/main/java/com/upiiz/examen_rpm_01/MainActivity.java
+++ app/src/main/java/com/upiiz/examen_rpm_01/MainActivity.java
@@ -1,6 +1,10 @@
 package com.upiiz.examen_rpm_01;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnAgregar1, btnActualizar1, btnEliminar1, btnCreditos1, btnSalir ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,51 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btnAgregar1 = findViewById(R.id.btnAgregar1);
+        btnActualizar1 = findViewById(R.id.btnActualizar1);
+        btnEliminar1 = findViewById(R.id.btnEliminar1);
+        btnCreditos1 = findViewById(R.id.btnCreditos1);
+        btnSalir = findViewById(R.id.btnSalir);
+
+        btnAgregar1.setOnClickListener(this);
+        btnActualizar1.setOnClickListener(this);
+        btnEliminar1.setOnClickListener(this);
+        btnCreditos1.setOnClickListener(this);
+        btnSalir.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v.getId()==btnSalir.getId()){
+            finish();
+        } else if (v.getId()==btnActualizar1.getId()) {
+            actualizar();
+        } else if (v.getId()==btnAgregar1.getId()) {
+            agregar();
+        } else if (v.getId()==btnCreditos1.getId()) {
+            creditos();
+        } else {
+            eliminar();
+        }
+
+    }
+
+    private void eliminar() {
+    }
+
+    private void creditos() {
+
+        Intent intentCreditsActivity = new Intent(this, CreditsActivity.class);
+        startActivity(intentCreditsActivity);
+
+    }
+
+    private void agregar() {
+    }
+
+    private void actualizar() {
     }
 }
